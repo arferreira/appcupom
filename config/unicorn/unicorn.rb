c54d14b22fe74ded6cb1741bb6a74e940220e60a
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-06-26 21:43:01 -0400

    config deploy

--- config/unicorn/unicorn.rb
+++ config/unicorn/unicorn.rb
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 
 # Set your full path to application.
-app_path = "/path/to/app"
+app_path = "/var/www/cupom/current"
 
 # Set unicorn options
 worker_processes 1
