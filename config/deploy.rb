6fe5c634edb6371e210ddfbf7bc685da2c4f31be
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-28 16:48:22 -0300

    ultimas mods

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,24 +12,18 @@ ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
 set :repository, 'git@github.com:arferreira/appcupom.git'
-
 set :rails_env, "production"
-
 set :branch, 'master'
 
-
 set :user, "root"
-
 set :use_sudo, false
 
 set :deploy_to, '/var/www/cupom'
 
-set :current, "#{deploy_to}/current"
 
 role :web, domain
 role :app, domain
 role :db,  domain, primary: true
-
 
 namespace :deploy do
   task :start do ; end
