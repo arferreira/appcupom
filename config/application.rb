be2f463d35841fc813ed96d4c3ac261f97b58c20
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-28 17:13:12 -0300

    mmiddleware

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,9 @@ module Appcupom
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    # config.active_record.whitelist_attributes = true
-
+    #config.active_record.whitelist_attributes = true
+    # config.serve_static_assets = true
+    # config.middleware.insert_before('Rack::Lock', '::API::Throttle')
+    config.middleware.insert_before(::Rack::Lock, ::ActionDispatch::Static, "#{root}/public")
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,11 @@
 require 'bundler/capistrano'
 
-set :domain, 'www.trazcupom.com'
-set :application, "appcupom"
+set :default_environment, {
+  :PATH => '/opt/local/bin:/opt/local/sbin:/opt/local/ruby/gems/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  :GEM_HOME => '/opt/local/ruby/gems'
+}
+
+set :application, '198.199.102.159'
 
 set :keep_releases, 3
 
@@ -12,18 +16,23 @@ ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
 set :repository, 'git@github.com:arferreira/appcupom.git'
-set :rails_env, "production"
+
 set :branch, 'master'
 
+set :deploy_via, :remote_cache
+
 set :user, "root"
+
 set :use_sudo, false
 
 set :deploy_to, '/var/www/cupom'
 
+set :current, "#{deploy_to}/current"
 
-role :web, domain
-role :app, domain
-role :db,  domain, primary: true
+role :web, application
+role :app, application
+role :db,  application, primary: true
+
 
 namespace :deploy do
   task :start do ; end
@@ -32,3 +41,4 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
