25fec310a1d1cab34fef52ff338d4d06d9a702cc
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-27 22:41:04 -0300

    added new facebook buttom login

--- app/assets/javascripts/partner_pics.js
+++
@@ -1,8 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-
-$(document).ready(function(){
-	 //setOnClickPartnerPicsHandler();
-	 setOnClickSelectHandler( ".partner_select_pic", "#selected_pics_input", "#num_select_input", "border_selected" )
-});
