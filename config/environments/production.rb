9f0ac82ca05f4d7cd951ae8cf163a65b3e2a03d7
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-28 16:34:54 -0300

    remove gambis from action_dispacth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,6 @@ Appcupom::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
-  config.middleware.insert_before(::Rack::Lock, ::ActionDispatch::Static, "#{root}/public")
-
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
