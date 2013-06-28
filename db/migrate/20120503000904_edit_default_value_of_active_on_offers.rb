4f88bae3549f1a021311f5c4c60271bba21f6902
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-28 19:24:01 -0300

    false inst 1

--- db/migrate/20120503000904_edit_default_value_of_active_on_offers.rb
+++ db/migrate/20120503000904_edit_default_value_of_active_on_offers.rb
@@ -1,6 +1,6 @@
 class EditDefaultValueOfActiveOnOffers < ActiveRecord::Migration
   def up
-    change_column_default(:offers, :active, 1)
+    change_column_default(:offers, :active, true)
   end
 
   def down
