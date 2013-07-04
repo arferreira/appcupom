0f6f4d1fc703922cc8209a819cf4fb10e1fbb1f2
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-07-03 20:25:29 -0400

    alteração preço cupom

--- vendor/mail_privacies.sql
+++ vendor/mail_privacies.sql
@@ -4,5 +4,5 @@ LIMIT 0, 1000
 
 -- Date: 2013-07-02 21:02
 */
-INSERT INTO `appcupom.mail_privacies` (`id`,`description`,`default`,`user_type`,`created_at`,`updated_at`) VALUES (1,'Receber email com ofertas semanais ',0,'U','2012-10-29 11:45:51','2012-10-29 11:45:51');
-INSERT INTO `appcupom.mail_privacies` (`id`,`description`,`default`,`user_type`,`created_at`,`updated_at`) VALUES (2,'Receber destaques do TrazCupom',1,'U','2012-10-29 11:45:51','2012-10-29 11:45:51');
+INSERT INTO `mail_privacies` (`id`,`description`,`default`,`user_type`,`created_at`,`updated_at`) VALUES (1,'Receber email com ofertas semanais ',0,'U','2012-10-29 11:45:51','2012-10-29 11:45:51');
+INSERT INTO `mail_privacies` (`id`,`description`,`default`,`user_type`,`created_at`,`updated_at`) VALUES (2,'Receber destaques do TrazCupom',1,'U','2012-10-29 11:45:51','2012-10-29 11:45:51');
--- vendor/privacies.sql
+++ vendor/privacies.sql
@@ -4,14 +4,14 @@ LIMIT 0, 1000
 
 -- Date: 2013-07-02 20:49
 */
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (1,'Compartilhar compra de oferta',0,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (2,'Desejar prato',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (3,'Comentar um desejo',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (4,'Recomendar um estabelecimento',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (5,'Recomendar um prato',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (6,'Comentar uma recomendação de estabelecimento',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (7,'Comentar uma recomendação de prato',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (8,'Receber Badge',1,'2012-11-16 16:45:04','2012-11-16 16:45:04','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (9,'Fazer amizades',1,'2012-11-16 16:45:09','2012-11-16 16:45:09','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (10,'Recomendar oferta',1,'2012-11-16 16:44:07','2012-11-16 16:44:07','U');
-INSERT INTO `appcupom.privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (11,'Comentar uma recomendação de oferta',1,'2012-11-16 16:44:15','2012-11-16 16:44:15','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (1,'Compartilhar cupom baixado',0,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (2,'Desejar produto',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (3,'Comentar um desejo',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (4,'Recomendar um estabelecimento',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (5,'Recomendar um cupom',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (6,'Comentar uma recomendação de estabelecimento',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (7,'Comentar uma recomendação de cupom',1,'2012-10-28 00:43:03','2012-10-28 00:43:03','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (8,'Receber Badge',1,'2012-11-16 16:45:04','2012-11-16 16:45:04','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (9,'Fazer amizades',1,'2012-11-16 16:45:09','2012-11-16 16:45:09','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (10,'Recomendar cupom',1,'2012-11-16 16:44:07','2012-11-16 16:44:07','U');
+INSERT INTO `privacies` (`id`,`description`,`default`,`created_at`,`updated_at`,`user_type`) VALUES (11,'Comentar uma recomendação de cupom',1,'2012-11-16 16:44:15','2012-11-16 16:44:15','U');
