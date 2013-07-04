44953eb6a6452cf79ec3a2a54e38926ebf2affab
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-04 01:55:18 -0300

    added some functions iin application.js

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -40,6 +40,44 @@ function frAdd(e) {
 function frRemove(e) {
   e.remove();
 }
+
+
+function OpenLink(theLink){
+  window.location.href = theLink.href;
+  return false;
+}
+
+function createGallery($param)
+{
+  (function(window, PhotoSwipe){
+    document.addEventListener('DOMContentLoaded', function(){
+      var
+      options = {},
+      instance = PhotoSwipe.attach( window.document.querySelectorAll($param + ' a'), options );
+    }, false);
+  }(window, window.Code.PhotoSwipe));
+}
+
+function initToggleList() {
+  $(".toggle-item .toggle-list").hide();
+}
+function initToggleList(_toggleItem, _toggleContent) {
+  // @_toggleItem   param o indice a ser clicado
+  // @_toggleContent  param o conteudo, filho de _toggleItem, a ser ocultado
+  
+  $(_toggleContent, _toggleItem).slideToggle();
+
+  $(_toggleItem).click(function(e) {
+    if ($(this).hasClass("opened")) {
+      $(this).removeClass("opened")
+    } else {
+      $(this).addClass("opened");
+    }
+
+    $(_toggleContent, this).slideToggle('fast');
+  })
+}
+
 var preventDoubleClick = false;
 function setOnClickSelectHandler(parent_class, output_id, max_select_input_id, toogle_class_id) {
 
@@ -749,6 +787,136 @@ function abre_modal(){
 }
 
 $(document).ready(function() {
+  //seleciona os elementos a com atributo name="modal"
+  $('a[name=modal]').live("click",  function(e) {
+    //cancela o comportamento padrão do link
+    e.preventDefault();
+
+    //armazena o atributo href do link
+    var id = $(this).attr('href');
+
+    //armazena a largura e a altura da tela
+    var maskHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+    var maskWidth = $(window).width();
+
+    //Define largura e altura do div#mask iguais ás dimensões da tela
+    $('#mask').css({
+      'width' : maskWidth,
+      'height' : maskHeight
+    });
+
+    //efeito de transição
+    $('#mask').fadeIn(500);
+    $('#mask').fadeTo(.4, 0.8);
+
+    //armazena a largura e a altura da janela
+    var winH = $(window).height();
+    var winW = $(window).width();
+    //centraliza na tela a janela popup
+    $(id).css('top', winH / 2 - $(id).height() / 2);
+    $(id).css('left', winW / 2 - $(id).width() / 2);
+    //efeito de transição
+    $(id).fadeIn(500);
+
+    $(window).bind('resize', function() {
+      //armazena a largura e a altura da janela
+      var winH = $(window).height();
+      var winW = $(window).width();
+      //centraliza na tela a janela popup
+      $(id).css('top', winH / 2 - $(id).height() / 2);
+      $(id).css('left', winW / 2 - $(id).width() / 2);
+      //armazena a largura e a altura da tela
+      var maskHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+      var maskWidth = $(window).width();
+      //Define largura e altura do div#mask iguais ás dimensões da tela
+      $('#mask').css({
+        'width' : maskWidth,
+        'height' : maskHeight
+      });
+    });
+
+  });
+
+  //se o botãoo fechar for clicado
+  $('.window .close').live("click",  function(e) {
+    //cancela o comportamento padrão do link
+    e.preventDefault();
+    $('#mask, .window').hide();
+    $(window).unbind('resize');
+  });
+
+  //se div#mask for clicado
+  $('#mask').live("click",  function() {
+    $(this).hide();
+    $('.window').hide();
+    $(window).unbind('resize');
+  });
+
+  //abrir informacoes da badge que esta habilitada
+  $(".badge-on").live("click",  function() {
+    var idBadge = $(this).attr("rel");
+    //pega o id da badge que esta no elemento rel
+    $(".list-badge").hide();
+    //esconde a listagem das badges
+    $(".badge-info").hide();
+    //esconde o titulo da modal
+
+    $(".badge" + idBadge + "").fadeIn();
+    //mostra a info sobre a badge clicada
+    $(".bt-back-modal").show();
+    //mostra a info sobre a badge clicada
+  })
+
+  $(".bt-back-modal").live("click",  function(e) {
+    $(".info-badge").hide();
+    //esconde a info sobre a badge clicada
+    $(".bt-back-modal").hide();
+    //esconde a info sobre a badge clicada
+    $(".badge-info").show();
+    //mostra o titulo da modal
+    $(".list-badge").show();
+    //mostra a listagem de badge
+  });
+
+  //verifica se existe a #index
+  var homescreen = $("#homescreen").css("display");
+  if (homescreen == "block") {
+    var delay = 5000;
+    if($("#login-container").length){
+      delay = 1000;
+    }
+    
+    $("#homescreen").delay(delay).animate({
+      opacity : 0,
+      //marginLeft : '-480px'
+    }, 1500, function() {
+      $(this).hide();
+      $("#index").fadeIn();
+    });
+  }
+
+  if (window.navigator.userAgent.indexOf('iPhone') != -1) {
+
+    if (window.navigator.standalone == true) {
+      //fullscreen
+      $("#homescreen-alert").hide();
+    } else {
+      $("#homescreen-alert").show();
+      $(".close-homescreen").bind("click", function() {
+        $("#homescreen-alert").hide();
+
+        $("#warning-home").slideDown();
+      });
+
+      $("#warning-home").bind("click", function() {
+        $("#warning-home").hide();
+
+        $("#homescreen-alert").fadeIn();
+      });
+    }
+
+  }
+
 if ($("#request_location").val() == "true") {
     // navigator.geolocation.getCurrentPosition(get_location_callback, get_location_error, {maximumAge:10000, enableHighAccuracy:true});
     m_startDate = new Date().getTime();
