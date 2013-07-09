f3e2aaf41c317a901d6e3b877140309fb8839e88
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-09 14:29:58 -0300

    adicionando segurança no sale tbm

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,7 +1,16 @@
 class SalesController < ApplicationController
+  before_filter :authenticate
   def index
   	@cupons = Cupon.all
-
-
+  end
+  
+  private
+  def authenticate
+    deny_access unless signed_in? ADMIN_TYPE
+  end
+  
+  def correct_user
+    @admin = Administrator.find(params[:id])
+    redirect_to(root_path, :notice => "Access Denied" )unless current_user?(@admin)
   end
 end
