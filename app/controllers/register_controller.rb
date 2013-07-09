c389bdfdae4c9c55c176a9228f284cdd23c52c51
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-09 14:27:37 -0300

    adicionando segurança

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,5 +1,18 @@
 class RegisterController < ApplicationController
+  before_filter :authenticate
   def index
   	@usuario = User.all
   end
+
+  private
+
+  def authenticate
+    deny_access unless signed_in? ADMIN_TYPE
+  end
+  
+  def correct_user
+    @admin = Administrator.find(params[:id])
+    redirect_to(root_path, :notice => "Access Denied" )unless current_user?(@admin)
+  end
+  
 end
